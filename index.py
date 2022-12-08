c3382e7fb802fd6f868f601e2f777edec1bef66a
Brian Williams <heffergod@gmail.com>
2022-12-07 21:40:57 -0800

    Streamlit grapher complete, still need to make look cleaner, and possibly make graphs larger

--- index.py
+++ index.py
@@ -5,36 +5,46 @@ import numpy as np
 import plotly.express as px
 import seaborn as sns
 
-df = pd.read_csv("airline_data_average.csv")
 
 
+def make_graph(df, selection, selection2, year_start, year_end):
+    
+    if year_start == "":
+        year_start = df.at[df.index[0], "year"]
+    else:
+        year_start = int(year_start)
 
-st.title("Airline Price Data Analysis")
+    if year_end == "":
+        year_end = df.at[df.index[-1], "year"]
+    else:
+        year_end = int(year_end)
 
-year_start = st.selectbox(
-    'Choose a starting year:', ('',1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)
-)
+    df = df.loc[(df["year"] >= year_start) & (df["year"] <= year_end)]
 
-year_end = st.selectbox(
-    'Choose an ending year:', ('',1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)
-)
-
-select = st.selectbox(
-    'Choose your first graph:', ('', 'distance', 'round_trip', 'total_covid_cases', 'total_covid_deaths', 'total_covid_tests_per_thousand', 'total_covid_vaccinations', 'oil_price_per_barrel', 'inflation_adjusted_itin_fare', 'inflation_adjusted_fare_per_mile')
-    )
-
-select2 = st.selectbox(
-    'Choose your second graph to overlay:', ('', 'distance', 'round_trip', 'total_covid_cases', 'total_covid_deaths', 'total_covid_tests_per_thousand', 'total_covid_vaccinations', 'oil_price_per_barrel', 'inflation_adjusted_itin_fare', 'inflation_adjusted_fare_per_mile')
-)
-
-
-def make_graph(selection, selection2):
-    if selection == '' or selection2 == '':
+    if selection == '' and selection2 == '':
         pass
+
+    elif selection2 == '':
+        fig, ax = plt.subplots(figsize=(20, 8))
+        # plt.title("Inflation Adjusted Fare Per Mile In Relation To Price Of Oil, From 1993 to 2022", fontsize = 30)
+        ax = sns.lineplot(data=df, y=f"{selection}", x =range(len(df)), color = "blue")
+        ax.tick_params(axis='y')
+        ax.set_xlabel("Year", fontsize = 18)
+        ax.set_ylabel(f"{selection}", fontsize = 18, color = "b")
+        length = len(df)
+        plt.xticks(np.arange(0, length, 4))
+
+        years = [str(i) for i in range(year_start, year_end+1)]
+
+        ax.set_xticklabels(years)
+        plt.tight_layout()
+
+        return fig
+
     else:
         fig, ax = plt.subplots(figsize=(20, 8))
         # plt.title("Inflation Adjusted Fare Per Mile In Relation To Price Of Oil, From 1993 to 2022", fontsize = 30)
-        ax = sns.lineplot(data=df, y=f"{selection}", x =range(len(df)), color = "blue");
+        ax = sns.lineplot(data=df, y=f"{selection}", x =range(len(df)), color = "blue")
         ax.tick_params(axis='y')
         ax.set_xlabel("Year", fontsize = 18)
         ax.set_ylabel(f"{selection}", fontsize = 18, color = "b")
@@ -42,13 +52,40 @@ def make_graph(selection, selection2):
         ax2 = sns.lineplot(data=df, y=f"{selection2}", x =range(len(df)), color = "red")
         ax2.tick_params(axis='y')
         ax2.set_ylabel(f"{selection2}", fontsize = 18, color = "r")
-        plt.xticks(np.arange(0, len(df)+1, 4))
+        length = len(df)+1
+        plt.xticks(np.arange(0, length, 4))
 
-        ax.set_xticklabels(["1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]);
+        years = [str(i) for i in range(year_start, year_end+1)]
+
+        ax.set_xticklabels(years)
         plt.tight_layout()
 
         return fig
-if select == '' or select2 == '':
-    pass
-else:
-    st.write(make_graph(select, select2))
+
+
+
+def main():    
+    df = pd.read_csv("airline_data_average.csv")
+
+    st.title("Airline Price Data Analysis")
+
+    years = ['']
+    years.extend(list(range(1993, 2023)))
+    year_start = st.selectbox('Choose a starting year:', years)
+    year_end = st.selectbox('Choose an ending year:', years)
+
+    select = st.selectbox(
+        'Choose your first graph:', ('', 'inflation_adjusted_fare_per_mile','inflation_adjusted_itin_fare', 'oil_price_per_barrel', 'total_covid_cases', 'total_covid_deaths', 'total_covid_tests_per_thousand', 'total_covid_vaccinations', 'distance', 'round_trip')
+        )
+
+    select2 = st.selectbox(
+        'Choose your second graph to overlay:', ('', 'inflation_adjusted_fare_per_mile','inflation_adjusted_itin_fare', 'oil_price_per_barrel', 'total_covid_cases', 'total_covid_deaths', 'total_covid_tests_per_thousand', 'total_covid_vaccinations', 'distance', 'round_trip')
+    )
+
+    if select == '' and select2 == '':
+        pass
+    else:
+        st.write(make_graph(df, select, select2, year_start, year_end))
+
+if __name__ == "__main__":
+    main()
